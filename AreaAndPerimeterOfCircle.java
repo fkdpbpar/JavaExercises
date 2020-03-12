0d53a74c4b417c67c1e0b2d4bf3129cc2da6af1b
fkdpbpar <62054451+fkdpbpar@users.noreply.github.com>
2020-03-12 21:07:29 +0200

    Add files via upload
    Write a Java program to print the area and perimeter of a circle. 
    Test Data:
    Radius = 7.5
    Expected Output
    Perimeter is = 47.12388980384689
    Area is = 176.71458676442586 
    Use memory-friendly variables and print the result with printf.

---
+++ AreaAndPerimeterOfCircle.java
@@ -0,0 +1,13 @@
+
+public class AreaAndPerimeterOfCircle {
+
+    
+    public static void main(String[] args) {
+    float Radius=7.5f;
+    double perimeter=(double)(2*Math.PI*Radius);
+    double area=(double)(Math.PI*Radius*Radius);
+    System.out.printf("The perimeter of circle with radius 7.5cm is %s cm%n",perimeter);
+    System.out.printf("The area of circle with radius 7.5cm is %s cm*cm %n",area);
+    }
+    
+}
